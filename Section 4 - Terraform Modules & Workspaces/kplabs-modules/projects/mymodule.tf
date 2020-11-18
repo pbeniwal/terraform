ffce7644a8bee304d0e87a8cf5dc284542e12aa9
pbeniwal <pbeniwal@yahoo.com>
2020-11-18 23:46:02 +0530

    Update mymodule.tf

--- Section 4 - Terraform Modules & Workspaces/kplabs-modules/projects/mymodule.tf
+++ Section 4 - Terraform Modules & Workspaces/kplabs-modules/projects/mymodule.tf
@@ -8,3 +8,20 @@ module "ec2module"{
     source = "../module/ec2"
     instance_type = "t2.large"
 }
+
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+
+  name                   = "my-cluster"
+  instance_count         = 1
+
+  ami                    = "ami-04bf6dcdc9ab498ca"
+  instance_type          = "t2.micro"
+  key_name               = "aws"
+  subnet_id              = "subnet-ecf62fcd"
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
